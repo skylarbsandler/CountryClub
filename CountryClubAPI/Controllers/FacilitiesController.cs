9c2db55edb09753c9dbbdd89a46237fcb2e3d793
skylarbsandler <skylarbsandler@gmail.com>
2023-10-09 14:18:40 -0600

    finished create bookings

---
+++ CountryClubAPI/Controllers/FacilitiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CountryClubAPI.Controllers
+{
+    public class FacilitiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
